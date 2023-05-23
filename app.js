1e076d8bc839117b8b66c9cac37767995d532bec
bilalkhattak55 <mbilalkhk@gmail.com>
2023-05-23 23:43:03 +0500

    created responsive navbar using html, css, js

---
+++ app.js
@@ -0,0 +1,17 @@
+// const navbarMenu = document.querySelector('.navbar-menu');
+// const navbarToggle = document.querySelector('.navbar-toggle');
+
+// navbarToggle.addEventListener('click', function() {
+//   navbarMenu.classList.toggle('collapse');
+// });
+
+const icons = document.querySelector('.icons-sec');
+const navbarLeft = document.querySelector('.navbar-left');
+const main_nav = document.querySelector('.main-navbar')
+
+
+icons.addEventListener('click', ()=> {
+ main_nav.classList.toggle('active')
+
+})
+
